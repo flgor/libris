6aa1439144bff61f5e7bcf119570859633d4038c
Florin Grozea <c7florin@gmail.com>
2017-11-26 20:16:52 +0200

    - Google analytics.

--- api/src/main/java/xyz/libris/api/pages/indexpage/IndexPageController.java
+++ api/src/main/java/xyz/libris/api/pages/indexpage/IndexPageController.java
@@ -1,11 +1,11 @@
-package xyz.libris.api.indexhtml;
+package xyz.libris.api.pages.indexpage;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-public class IndexHtmlController {
+public class IndexPageController {
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String init() {
---
+++ api/src/main/java/xyz/libris/api/pages/login/LoginPageController.java
@@ -0,0 +1,7 @@
+package xyz.libris.api.pages.login;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class LoginPageController {
+}
